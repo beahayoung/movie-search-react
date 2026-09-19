f5d6cf32ec17095553bf51d2a25125cd5a9cf92e
hayoung <bhy5635@gmail.com>
2026-09-19 23:38:22 +0900

    회원가입 퍼블리싱

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,54 +1,77 @@
-import React, { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
+import { faBolt, faCircleXmark, faL } from '@fortawesome/free-solid-svg-icons';
 
 const Signup = () => {
-    const [nickname, setNickname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
+    const inputRef = useRef(null);
+    
+    const steps = [
+        { name: "nickname", label: "닉네임", type: "text", placeholder: "닉네임", alert:"8자 이내로 입력해주세요.", maxLen:8},
+        { name: "email", label: "이메일", type: "email", placeholder: "이메일", alert:"이메일 형식으로 입력해주세요.", maxLen:50},
+        { name: "password", label: "비밀번호", type: "password", placeholder: "비밀번호 입력", alert:"8자 이내로 입력해주세요.", maxLen:8},
+        { name: "confirmPassword", label: "비밀번호 확인", type: "password", placeholder: "비밀번호을 한 번 더 입력해주세요", alert:"비밀번호 같이 입력해주세요", maxLen:8},
+    ]
+    const [formData, setFormData] = useState({
+        nickname: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    })
+    const [step, setStep] = useState(0);
+    const [disable, setdisable] = useState(true);
+    useEffect(()=> {
+        if(inputRef.current) {
+            inputRef.current.focus();
+            setdisable(true)
+        }
+    }, [step])
+    const valueForm = (name, value) => {
+        setFormData({...formData, [name]:value});
+        setdisable(false)
+    }
+    const onBlurhanedle = (length) => {
+        if(length <=0) {
+            setdisable(true);
+        } else {
+            setdisable(false);
+        }
     }
     return (
         <div className='auth-page'>
-                <div className='vort_logo'>
+            <div className='vort_logo'>
                 <Link to="/">
                     <FontAwesomeIcon icon={faBolt} style={{ color: '#7c5cea' }} />VOLT
                 </Link>
             </div>
             <div className='auth-card'>
-                <h2 className='auth-tit'>닉네임 입력해주세요.</h2>
-                <form className='auth-form' onSubmit={handleSubmit}>
-                    <div className='input-group'>
-                        <label htmlFor='nickname'>닉네임</label>
-                        <div className='input_wrap'>
-                        <input id='nickname' type='text' placeholder='표시될 이름' value={nickname} onChange={(e) => setNickname(e.target.value)} required />
-                        <p className='alert'>8자 이내로 입력해주세요.</p>
-                        </div>
-                    </div>
-                    <div className='input-group'>
-                        <label htmlFor='email'>이메일</label>
-                        <input id='email' type='email' placeholder='you@example.com' value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    </div>
-                    <div className='input-group'>
-                        <label htmlFor='password'>비밀번호</label>
-                        <input id='password' type='password' placeholder='비밀번호 입력' value={password} onChange={(e) => setPassword(e.target.value)} required />
-                    </div>
-                    <div className='input-group'>
-                        <label htmlFor='confirmPassword'>비밀번호 확인</label>
-                        <input id='confirmPassword' type='password' placeholder='비밀번호을 한 번 더 입력해주세요' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
-                    </div>
+                <h2 className='auth-tit'>{steps[step].label} 입력해주세요.</h2>
+                <form className='auth-form' >
+                    {
+                        steps.slice(0, step + 1).map((item, index) => (
+                            <div className={`input-group ${index<step?"completed":""}`} key={item.name}>
+                                <label htmlFor={item.name} className='input_name'>{item.label}</label>
+                                <div className='input_group_wrap'>
+                                    <div className='input_wrap'>
+                                    <input id={item.name} type={item.type} ref={index === step ? inputRef :null} maxLength={item.maxLen} name={item.name} placeholder={item.placeholder} value={formData[item.name]} onChange={(e)=>valueForm(item.name,e.target.value)} required onBlur={(e)=>onBlurhanedle(e.target.value.length)}/>
+                                    <button type='button' className='input_clear_btn' onMouseDown={(e) => e.preventDefault()} onClick={()=>setFormData({...formData, [item.name]:""})}>
+                                        <FontAwesomeIcon icon={faCircleXmark} />
+                                    </button>
+                                    </div>
+                                    <p className='alert'>{item.alert}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                     <div className='button_wrap'>
-                        <button type='button' className='auth-submit'>이전</button>
-                        <button type='button' className='auth-submit'>다음</button>
-                        <button type='submit' className='auth-submit'>가입 하기</button>
+                        
+                        {step === steps.length-1 ?
+                        <button type='submit' className='auth-submit' disabled={disable}>가입 하기</button>:<button type='button' disabled={disable} className='auth-submit next' onClick={() => setStep(step + 1)}>다음</button>
+                        }
                     </div>
                 </form>
             </div>
-                <p className='auth-switch'>이미 계정이 있으신가요? <Link to='/login'>로그인</Link></p>
+            <p className='auth-switch'>이미 계정이 있으신가요? <Link to='/login'>로그인</Link></p>
         </div>
     )
 };
